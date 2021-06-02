2d9f2690445d94740a9202c41855ff8d51f6f8c0
Diego <diegoteixeirapereira@hotmail.com>
2021-06-02 14:28:34 -0300

    criação dos arquivos java de inicialização.

---
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/SistemaDeVendasSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVendasSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVendasSpringBootApplication.class, args);
+	}
+
+}
