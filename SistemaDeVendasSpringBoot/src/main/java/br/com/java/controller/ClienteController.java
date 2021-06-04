1b734d66d7d67f662b7e0567f36e99ba2c772bc6
Diego <diegoteixeirapereira@hotmail.com>
2021-06-04 09:59:59 -0300

    criação do arquivo classe controller cliente.

---
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/ClienteController.java
@@ -0,0 +1,16 @@
+package br.com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cadastros/cliente")
+public class ClienteController {
+	
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "cadastros/cliente/CadCliente";
+	}
+
+}
