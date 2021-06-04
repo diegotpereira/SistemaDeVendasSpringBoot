440e241a1b715a9fa2a3d90ecbd5c136202b87d1
Diego <diegoteixeirapereira@hotmail.com>
2021-06-04 15:48:56 -0300

    criação dos arquivos de cadastro e consulta de produtos.

---
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/ProdutoController.java
@@ -0,0 +1,20 @@
+package br.com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cadastros/produto")
+public class ProdutoController {
+	
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "cadastros/produto/CadProduto";
+	}
+	
+	@GetMapping("/consultar")
+	public String consultar() {
+		return "cadastros/produto/ConsProduto";
+	}
+}
