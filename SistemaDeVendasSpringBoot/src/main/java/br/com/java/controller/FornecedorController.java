9b97b654a05b279248817eaf9c6c3ea6992120aa
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-04 12:26:19 -0300

    criação do arquivo classe java controle de fornecedor.

---
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/FornecedorController.java
@@ -0,0 +1,15 @@
+package br.com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cadastros/fornecedor")
+public class FornecedorController {
+
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "cadastros/fornecedor/CadFornecedor";
+	}
+}
