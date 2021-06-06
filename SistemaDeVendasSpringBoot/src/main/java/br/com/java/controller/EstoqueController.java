a8d8bf5e538b3bc794bffc9839fb5bd2f792c02d
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-06-06 15:38:06 -0300

    criação do arquivo template estoque.

--- SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/EstoqueController.java
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/EstoqueController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/estoque")
 public class EstoqueController {
 	
 	@GetMapping("/estoque")
 	public String estoque() {
-		return "estoque";
+		return "estoque/estoque";
 	}
 }
