e68deb96ddbdd61da26bac8ca21e2aec9946cd42
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-06-06 15:39:13 -0300

    alteração no arquivo controle vendas.

--- SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/VendasController.java
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/VendasController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class VendasController {
 	
-	@GetMapping("vendas")
+	@GetMapping("/vendas")
 	public String vendas() {
-		return "/vendas";
+		return "vendas";
 	}
 }
