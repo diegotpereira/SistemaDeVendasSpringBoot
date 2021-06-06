f56331acd8707936dbcbfa976d6a15addfb4ba06
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-06-06 18:07:16 -0300

    criação arquivos classes modelo java.

---
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/model/Cliente.java
@@ -0,0 +1,93 @@
+package br.com.java.model;
+
+import javax.persistence.*;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente extends AbstractEntity<Long>{
+	
+	private String nome;
+	private String endereco;
+	private String bairro;
+	private String numero;
+	private String cidade;
+	private String email;
+	private String telefone;
+	private String cep;
+	
+	@Enumerated(EnumType.STRING)
+	private UF uf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public UF getUf() {
+		return uf;
+	}
+
+	public void setUf(UF uf) {
+		this.uf = uf;
+	}
+}
