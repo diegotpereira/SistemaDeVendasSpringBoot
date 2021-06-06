0e8c6884eabdc148d2cb836eb9b6de6aa345e0f2
Diego Pereira <diegoteixeirapereira@hotmail.com>
2021-06-06 13:58:23 -0300

    criação do template estoque e suporte html.

---
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/SuporteController.java
@@ -0,0 +1,15 @@
+package br.com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class SuporteController {
+	
+	@GetMapping("/suporte")
+	public String suporte() {
+		return "suporte";
+	}
+}
