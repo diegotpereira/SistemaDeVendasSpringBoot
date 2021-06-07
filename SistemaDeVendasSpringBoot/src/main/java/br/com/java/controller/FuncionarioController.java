8482427db47993e851ae26ac3a8615d23d1c2ad1
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-07 17:15:46 -0300

    alteração nos arquivos templates Cad e Cons funcionarios para cadastro e consulta.

--- SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/FuncionarioController.java
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/FuncionarioController.java
@@ -1,20 +1,69 @@
 package br.com.java.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.java.model.Funcionario;
+import br.com.java.model.UF;
+import br.com.java.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/cadastros/funcionario")
 public class FuncionarioController {
 	
+	@Autowired
+	private FuncionarioService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "cadastros/funcionario/CadFuncionario";
 	}
 	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario) {
+		service.salvar(funcionario);
+		
+		return "redirect:/cadastros/funcionario/cadastrar";
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		
+		return UF.values();
+	}
+	
 	@GetMapping("consultar")
-	public String consultar() {
+	public String consultar(ModelMap model) {
+		model.addAttribute("funcionarios", service.buscarTodos());
 		return "cadastros/funcionario/ConsFuncionario";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("funcionario", service.buscarPorId(id));
+		
+		return "cadastros/funcionario/CadFuncionario";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		service.editar(funcionario);
+		attr.addFlashAttribute("sucesso", "Funcionário atualizado com sucesso!.");
+		
+		return "redirect:/cadastros/funcionario/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id) {
+		service.excluir(id);
+		
+		return "redirect:/cadastros/funcionario/consultar";
 	}
 }
