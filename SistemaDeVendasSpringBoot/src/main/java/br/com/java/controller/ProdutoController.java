b35498831e303b880ecee7b83a03ea17103f4e9c
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-09 11:33:55 -0300

    alteração no arquivo controle produto adição metodo para selecionar fornecedor.

--- SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/ProdutoController.java
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/ProdutoController.java
@@ -1,7 +1,10 @@
 package br.com.java.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,9 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.java.model.Fornecedor;
 import br.com.java.model.GRUPO;
 import br.com.java.model.Produto;
 import br.com.java.model.SUBGRUPO;
+import br.com.java.model.UF;
 import br.com.java.service.FornecedorService;
 import br.com.java.service.ProdutoService;
 
@@ -51,10 +56,17 @@ public class ProdutoController {
 	@GetMapping("/consultar")
 	public String consultar(ModelMap model) {
 		model.addAttribute("produtos", service.buscarTodos());
-		model.addAttribute("fornecedor", serv.buscarTodos());
+//		model.addAttribute("listaFornecedor", serv.buscarTodos());
 		
 		return "cadastros/produto/ConsProduto";
 	}
+	
+	
+	@ModelAttribute("listaFornecedor")
+	   private String listFornecedor(Model model){
+	      model.addAttribute("listaFornecedor", serv.buscarTodos());
+	      return "listaFornecedor";
+	  	}
 	
 	@GetMapping("/editar/{id}")
 	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
