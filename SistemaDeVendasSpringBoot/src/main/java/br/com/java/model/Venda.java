87aaed3eb40d4dc6b0232a14f5671abdea55fd17
Diego <diegoteixeirapereira@hotmail.com>
2021-06-10 16:53:59 -0300

    alteração no arquivo classe java venda modificação de atributos.

--- SistemaDeVendasSpringBoot/src/main/java/br/com/java/model/Venda.java
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/model/Venda.java
@@ -1,9 +1,20 @@
 package br.com.java.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.format.annotation.NumberFormat.Style;
 
 @SuppressWarnings("serial")
 @Entity
@@ -12,22 +23,79 @@ public class Venda extends AbstractEntity<Long>{
 	
 	private String codigoVenda;
 	
+	@DateTimeFormat(iso = ISO.DATE)
+	@Column(name = "dataPedido", nullable = false, columnDefinition = "DATE")
+	private LocalDate dataPedido;
+	
+//	@Column(name = "formaPagamento", nullable = false, length = 50)
+//	@Enumerated(EnumType.STRING)
+//	private Pagamento formaPagamento;
+	
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	@Column(nullable = false, columnDefinition = "DECIMAL(7) DEFAULT 0.00")
+	private BigDecimal desconto;
+	
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	@Column(nullable = false, columnDefinition = "DECIMAL(7) DEFAULT 0.00")
+	private BigDecimal total;
+	
 	@ManyToOne
-	@JoinColumn(name = "idCliente")
+	@JoinColumn(name = "id_funcionario")
+	private Funcionario funcionario;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
 	private Cliente cliente;
 	
-	@ManyToOne
-	@JoinColumn(name = "idProduto")
-	private Produto produto;
+	@OneToMany(mappedBy = "pedido")
+	private List<Pedido> listaDeProdutos;
 	
-	private Integer quantidade;
-
 	public String getCodigoVenda() {
 		return codigoVenda;
 	}
 
 	public void setCodigoVenda(String codigoVenda) {
 		this.codigoVenda = codigoVenda;
+	}
+
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+//	public Pagamento getFormaPagamento() {
+//		return formaPagamento;
+//	}
+//
+//	public void setFormaPagamento(Pagamento formaPagamento) {
+//		this.formaPagamento = formaPagamento;
+//	}
+
+	public BigDecimal getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(BigDecimal desconto) {
+		this.desconto = desconto;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
 	}
 
 	public Cliente getCliente() {
@@ -38,19 +106,11 @@ public class Venda extends AbstractEntity<Long>{
 		this.cliente = cliente;
 	}
 
-	public Produto getProduto() {
-		return produto;
+	public List<Pedido> getListaDeProdutos() {
+		return listaDeProdutos;
 	}
 
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-
-	public Integer getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
+	public void setListaDeProdutos(List<Pedido> listaDeProdutos) {
+		this.listaDeProdutos = listaDeProdutos;
 	}
 }
