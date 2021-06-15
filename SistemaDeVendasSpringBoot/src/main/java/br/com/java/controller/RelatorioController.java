9e5edae49dca7e04f14067b716d27c0e9dedaeb6
Diego <diegoteixeirapereira@hotmail.com>
2021-06-15 10:02:45 -0300

    alteração no arquivo classe controle de ralatório adicionado parametros.

--- SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/RelatorioController.java
+++ SistemaDeVendasSpringBoot/src/main/java/br/com/java/controller/RelatorioController.java
@@ -1,15 +1,58 @@
 package br.com.java.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 @Controller
 @RequestMapping("/relatorios")
 public class RelatorioController {
-    
-    @GetMapping("/show")
-    public String cadastrar() {
-        return "/relatorios/exemplojm.jrxml";
-    }
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@PostMapping
+	public void imprimir(@RequestParam Map<String, Object> parametros, HttpServletResponse response) throws JRException, SQLException, IOException{
+		
+		parametros = parametros == null ? parametros = new HashMap<>() : parametros;
+		
+		// Pega o arquivo .jasper localizado na pasta resources
+		InputStream jasperStream = this.getClass().getResourceAsStream("/relatorios/cliente.jasper");
+		
+		// Cria o objeto JasperReport com o Stream do arquivo jasper da pasta resource.
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+		
+		// Passa para o JasperPrint o relatório, os parâmetros e a fonte dos dados, 
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperStream, parametros, dataSource.getConnection());
+		
+		// Configura a reposta para o tipo PDF
+		response.setContentType("application/pdf");
+		
+		// Define que o arquivo pode ser visualizado no navegador e também nome final do arquivo
+		// para fazer download do relatório troque 'inline' por 'attachment'
+		response.setHeader("Content-Disposition", "inline; filename = ClienteService.pdf");
+		
+		// Faz a exportação do relatório para o HttpServletResponse
+		final OutputStream outStream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+	}
 }
